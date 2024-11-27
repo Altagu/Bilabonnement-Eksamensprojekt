e7847c4c43cb26017ed8bc335a9fc00de9e3bd7a
Altagu <alexgunnersen25@gmail.com>
2024-11-27 12:48:47 +0100

    Test

---
+++ src/main/java/com/example/bilabonnementeksamensprojekt/BilabonnementEksamensprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnementeksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilabonnementEksamensprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilabonnementEksamensprojektApplication.class, args);
+    }
+
+}
